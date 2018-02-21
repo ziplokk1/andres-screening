6eb248e1da92106d54f96e00d555377b697ad6c1
Mark Sanders <ziplokk.mark.sanders@gmail.com>
2018-02-21 15:14:05 -0600

    Finished screening.

--- scala/screening/src/main/scala/main.scala
+++ scala/screening/src/main/scala/main.scala
@@ -1,7 +1,92 @@
 package scala;
 
-import java.io.File
+import java.io.File;
+import java.nio.charset.CodingErrorAction
+import scala.collection.mutable.ListBuffer;
+import scala.io.Source;
+import scala.io.Codec;
 
-object Main extends App {
-  println("hello")
+class Application {
+
+  private val decoder = Codec.UTF8.decoder.onMalformedInput(CodingErrorAction.IGNORE)
+
+  def run(dir: String) {
+    val filePathsBuffer = ListBuffer[String]();
+    listCsvFiles(dir, filePathsBuffer);
+    val filePaths = filePathsBuffer.toList;
+
+    val averageCols = averageColumns(filePaths);
+    println(s"Average Columns: ${averageCols}");
+
+    val numRows = numberOfRows(filePaths);
+    println(s"Number Of Rows: ${numRows}");
+
+    val counts = wordCount(filePaths);
+    println("Word Counts:");
+    for (l <- counts.keys) {
+      print(l);
+      print(',');
+      println(counts(l));
+    }
+  }
+
+  def wordCount(filePaths: List[String]) : Map[String,Int] = {
+    var m = Map[String,Int]();
+    for (filePath <- filePaths) {
+      val f = Source.fromFile(filePath)(decoder);
+      for (line <- f.getLines()) {
+        val l = line.replaceAll("[\"]", "");
+        val arr = l.split("[\\s+,]");
+        for (s <- arr) {
+          val count = (if (m.contains(s)) m(s) else 0) + 1;
+          m += (s -> count);
+        }
+      }
+    }
+    return m;
+  }
+
+  def averageColumns(filePaths: List[String]) : Int = {
+    val numFiles = filePaths.length;
+    var numColumns = 0;
+    for (filePath <- filePaths) {
+      val f = Source.fromFile(filePath)(decoder);
+      val line = f.getLines().take(1).toList(0);
+      f.close;
+      numColumns += line.split(",").length;
+    }
+    return numColumns / numFiles;
+  }
+
+  def numberOfRows(filePaths: List[String]) : Int = {
+    var rowSum = 0;
+    for (filePath <- filePaths) {
+      val f = Source.fromFile(filePath)(decoder);
+      for (line <- f.getLines()) {
+        rowSum += 1;
+      }
+      f.close();
+    }
+    return rowSum;
+  }
+
+  def listCsvFiles(dir: String, out: ListBuffer[String]) {
+    val folder = new File(dir);
+    for (file <- folder.listFiles()) {
+      val path = file.getAbsolutePath();
+      if (file.isFile() && path.endsWith(".csv")) {
+        out += path;
+      }
+      if (file.isDirectory()) {
+        listCsvFiles(path, out);
+      }
+    }
+  }
+}
+
+object Main {
+  def main(args: Array[String]) {
+    val app = new Application();
+    app.run("/root/data");
+  }
 }
